110978018a15c2058f82ff9224b19dec24333d4c
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-30 11:25:16 -0600

    .

--- target/generated-sources/annotations/com/grid/catalog/converters/ArticleMapperImpl.java
+++ target/generated-sources/annotations/com/grid/catalog/converters/ArticleMapperImpl.java
@@ -1,14 +1,16 @@
 package com.grid.catalog.converters;
 
 import com.grid.catalog.models.Article;
+import com.grid.catalog.models.Article.ArticleBuilder;
 import com.grid.catalog.models.dtos.ArticleDto;
+import com.grid.catalog.models.dtos.ArticleDto.ArticleDtoBuilder;
 import java.math.BigDecimal;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-24T10:58:04-0600",
+    date = "2023-03-28T15:48:58-0600",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 19.0.1 (Homebrew)"
 )
 @Component
@@ -20,27 +22,24 @@ public class ArticleMapperImpl implements ArticleMapper {
             return null;
         }
 
-        String uniqId = null;
+        ArticleDtoBuilder articleDto = ArticleDto.builder();
 
-        uniqId = article.getUniqId();
-
-        ArticleDto articleDto = new ArticleDto( uniqId );
-
-        articleDto.setId( article.getId() );
-        articleDto.setSku( article.getSku() );
-        articleDto.setName_title( article.getName_title() );
-        articleDto.setDescription( article.getDescription() );
+        articleDto.id( article.getId() );
+        articleDto.uniqId( article.getUniqId() );
+        articleDto.sku( article.getSku() );
+        articleDto.name_title( article.getName_title() );
+        articleDto.description( article.getDescription() );
         if ( article.getList_price() != null ) {
-            articleDto.setList_price( new BigDecimal( article.getList_price() ) );
+            articleDto.list_price( new BigDecimal( article.getList_price() ) );
         }
-        articleDto.setSale_price( article.getSale_price() );
-        articleDto.setCategory( article.getCategory() );
-        articleDto.setCategory_tree( article.getCategory_tree() );
-        articleDto.setAverage_product_rating( article.getAverage_product_rating() );
-        articleDto.setProduct_url( article.getProduct_url() );
-        articleDto.setBrand( article.getBrand() );
+        articleDto.sale_price( article.getSale_price() );
+        articleDto.category( article.getCategory() );
+        articleDto.category_tree( article.getCategory_tree() );
+        articleDto.average_product_rating( article.getAverage_product_rating() );
+        articleDto.product_url( article.getProduct_url() );
+        articleDto.brand( article.getBrand() );
 
-        return articleDto;
+        return articleDto.build();
     }
 
     @Override
@@ -49,23 +48,23 @@ public class ArticleMapperImpl implements ArticleMapper {
             return null;
         }
 
-        Article article = new Article();
+        ArticleBuilder article = Article.builder();
 
-        article.setId( articleDto.getId() );
-        article.setUniqId( articleDto.getUniqId() );
-        article.setSku( articleDto.getSku() );
-        article.setName_title( articleDto.getName_title() );
-        article.setDescription( articleDto.getDescription() );
+        article.id( articleDto.getId() );
+        article.uniqId( articleDto.getUniqId() );
+        article.sku( articleDto.getSku() );
+        article.name_title( articleDto.getName_title() );
+        article.description( articleDto.getDescription() );
         if ( articleDto.getList_price() != null ) {
-            article.setList_price( articleDto.getList_price().toString() );
+            article.list_price( articleDto.getList_price().toString() );
         }
-        article.setSale_price( articleDto.getSale_price() );
-        article.setCategory( articleDto.getCategory() );
-        article.setCategory_tree( articleDto.getCategory_tree() );
-        article.setAverage_product_rating( articleDto.getAverage_product_rating() );
-        article.setProduct_url( articleDto.getProduct_url() );
-        article.setBrand( articleDto.getBrand() );
+        article.sale_price( articleDto.getSale_price() );
+        article.category( articleDto.getCategory() );
+        article.category_tree( articleDto.getCategory_tree() );
+        article.average_product_rating( articleDto.getAverage_product_rating() );
+        article.product_url( articleDto.getProduct_url() );
+        article.brand( articleDto.getBrand() );
 
-        return article;
+        return article.build();
     }
 }
